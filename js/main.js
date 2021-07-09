1494259d182c668bb2df9384bb19bae90340efb2
kozimov786 <email@youremail.com>
2021-07-09 14:42:26 +0500

    add contact and about page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-let burger = document.querySelector('.burger');
-let sitenav = document.querySelector('.sitenav');
+let burger = document.querySelector(".burger");
+let sitenav = document.querySelector(".sitenav");
 
-burger.addEventListener('click',()=>{
-  sitenav.classList.toggle('active');
-  burger.classList.toggle('is-open');
+burger.addEventListener("click", () => {
+  sitenav.classList.toggle("active");
+  burger.classList.toggle("is-open");
+});
+
+let direcBtn = document.querySelectorAll(".directors__btn");
+
+direcBtn.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentElement.classList.toggle("directors__item--quote");
+    item.classList.toggle("directors__btn--close");
+  });
 });
